5bda363ad765bf8a5acb766d964f83a0b488edcc
evizstara <evzi.stara@gmail.com>
2025-04-24 15:48:13 +0200

    Fix makrdown

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -7,7 +7,7 @@ function Recipe(props){
     return(
 <div className="bg-white rounded-xl p-5">
 
-<div>
+<div className="prose max-w-none">
     <h2 className="text-3xl">Suggested recipe</h2>
         <ReactMarkdown>{props.recipeData}</ReactMarkdown>
       </div>
